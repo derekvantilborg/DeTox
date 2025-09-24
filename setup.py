f6ca79de121f261a31332a7710e920b7f653c5ce
derekvantilborg <dwvantilborg@gmail.com>
2025-09-24 11:33:39 +0200

    added some inits and setup, changed default config mlp

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="detox",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "torch",
+        "pytorch-lightning",
+        "pandas",
+        "numpy",
+        "rdkit",
+        "tqdm",
+        "pyyaml",
+        "PyTDC",
+    ],
+)
